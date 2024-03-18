446512c5e2cb24ab65e257ca6f5b67ae5c698ac0
dhanushgit72 <dhanushdouda711@gmail.com>
2024-03-18 22:19:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch("https://icanhazdadjoke.com/slack")
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById("jokeElement");
+        jokeElement.innerHTML = jokeText;
+
+    })
+
+const button = document.getElementById("button");
+button.addEventListener("click", function () {
+    fetch("https://icanhazdadjoke.com/slack")
+        .then(data => data.json())
+        .then(jokeData => {
+            const jokeText = jokeData.attachments[0].text;
+            const jokeElement = document.getElementById("jokeElement");
+            jokeElement.innerHTML = jokeText;
+
+        })
+
+});
